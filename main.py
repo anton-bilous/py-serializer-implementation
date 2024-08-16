5a6f44d2e121997dfb319d402e4d8151e3a2ba2b
Anton Bilous <anton.bilosus@gmail.com>
2024-08-16 19:40:32 +0300

    Implement deserialize_car_object

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
 from car.models import Car
@@ -9,4 +12,6 @@ def serialize_car_object(car: Car) -> bytes:
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    serializer = CarSerializer(data=JSONParser().parse(BytesIO(json)))
+    serializer.is_valid(raise_exception=True)
+    return serializer.create(serializer.validated_data)
